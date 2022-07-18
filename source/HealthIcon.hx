4a97b540df1a6e7895770559f1f159717e411a4d
JuniorNovoa1 <87715171+JuniorNovoa1@users.noreply.github.com>
2022-07-17 21:46:37 -0400

    revert back because stolen code

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -1,59 +1,38 @@
 package;
 
 import flixel.FlxSprite;
-import openfl.utils.Assets as OpenFlAssets;
-
-using StringTools;
 
 class HealthIcon extends FlxSprite
 {
 	public var sprTracker:FlxSprite;
-	private var isOldIcon:Bool = false;
-	private var isPlayer:Bool = false;
-	private var char:String = '';
 
 	public function new(char:String = 'bf', isPlayer:Bool = false)
 	{
 		super();
-		isOldIcon = (char == 'bf-old');
-		this.isPlayer = isPlayer;
-		changeIcon(char);
-		scrollFactor.set();
-	}
+		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
 
-	private var iconOffsets:Array<Float> = [0, 0];
-	public function changeIcon(char:String) {
-		if(this.char != char) {
-			var name:String = 'icons/' + char;
-			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char;
-			var file:Dynamic = Paths.image(name);
-
-			loadGraphic(file); //Load stupidly first for getting the file size
-			loadGraphic(file, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr
-			iconOffsets[0] = (width - 150) / 2;
-			iconOffsets[1] = (width - 150) / 2;
-			updateHitbox();
-
-			animation.add(char, [0, 1], 0, false, isPlayer);
-			animation.play(char);
-			this.char = char;
-
-			antialiasing = true;
-			if(char.endsWith('-pixel')) {
-				antialiasing = false;
-			}
-		}
-	}
-
-	override function updateHitbox()
-	{
-		super.updateHitbox();
-		offset.x = iconOffsets[0];
-		offset.y = iconOffsets[1];
-	}
-
-	public function getCharacter():String {
-		return char;
+		antialiasing = true;
+		animation.add('bf', [0, 1, 24], 0, false, isPlayer);
+		animation.add('bf-car', [0, 1], 0, false, isPlayer);
+		animation.add('bf-christmas', [0, 1], 0, false, isPlayer);
+		animation.add('bf-pixel', [21, 21], 0, false, isPlayer);
+		animation.add('bf-holding-gf', [0, 1, 24], 0, false, isPlayer);
+		animation.add('spooky', [2, 3], 0, false, isPlayer);
+		animation.add('pico', [4, 5], 0, false, isPlayer);
+		animation.add('mom', [6, 7], 0, false, isPlayer);
+		animation.add('mom-car', [6, 7], 0, false, isPlayer);
+		animation.add('tankman', [8, 9], 0, false, isPlayer);
+		animation.add('face', [10, 11], 0, false, isPlayer);
+		animation.add('dad', [12, 13], 0, false, isPlayer);
+		animation.add('senpai', [22, 22], 0, false, isPlayer);
+		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
+		animation.add('spirit', [23, 23], 0, false, isPlayer);
+		animation.add('bf-old', [14, 15], 0, false, isPlayer);
+		animation.add('gf', [16], 0, false, isPlayer);
+		animation.add('parents-christmas', [17], 0, false, isPlayer);
+		animation.add('monster', [19, 20], 0, false, isPlayer);
+		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
+		animation.play(char);
 	}
 
 	override function update(elapsed:Float)
